d7c1e49a27f41b396d4cd07fcbde170bf664ef88
kerryspchang <kerryspchang@gmail.com>
2018-04-04 22:57:28 -0400

    initial upload

---
+++ composition.js
@@ -0,0 +1,19 @@
+let {projectName} = require('./shellConfig.js');
+
+composer.try(
+	composer.sequence(
+		`/${projectName}/watson-language/languageId`,
+		composer.if(
+			p => p.language !== 'en',
+			composer.sequence(
+				p => ({translateFrom: p.language, translateTo: 'en', payload: p.payload}),
+				`/${projectName}/watson-language/translator`
+			),
+			composer.sequence(
+				p => ({text: p.payload}),
+				`/${projectName}/en2shakespeare`
+			)			
+		)
+	),
+	err => ({payload:'Sorry we cannot translate your text'})
+)
---
+++ shellConfig.js
@@ -0,0 +1,13 @@
+{
+	"Fun Translations": {
+		"instructions": "This example uses the Shakespeare API from Fun Translations. Enter the API Key below. If you do not enter a key, the usage limit is 5 calls/day.",
+		"required": false,
+		"source": "http://funtranslations.com/api#shakespeare",
+		"input": [{"name": "ftApiKey", "label": "API Key"}]
+	},
+	"Watson Translations": {
+		"instructions": "Deploying this example will automatically create a IBM Watson Translation service under the free lite plan.",
+		"source": "https://www.ibm.com/watson/services/language-translator/"
+	}
+
+}
