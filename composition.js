3c5dc1745ccd0b196682c29d3ec65743469c2460
kerryspchang <kerryspchang@gmail.com>
2018-04-04 23:48:47 -0400

    Update composition.js

--- composition.js
+++ composition.js
@@ -1,15 +1,15 @@
 composer.try(
 	composer.sequence(
-		`/${projectName}/watson-language/languageId`,
+		`/${namespace}/watson-language/languageId`,
 		composer.if(
 			p => p.language !== 'en',
 			composer.sequence(
 				p => ({translateFrom: p.language, translateTo: 'en', payload: p.payload}),
-				`/${projectName}/watson-language/translator`
+				`/${namespace}/watson-language/translator`
 			),
 			composer.sequence(
 				p => ({text: p.payload}),
-				`/${projectName}/en2shakespeare`
+				`/${namespace}/${projectName}/en2shakespeare`
 			)			
 		)
 	),
