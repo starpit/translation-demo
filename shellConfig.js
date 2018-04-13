cebebe60561aed12a9728eb7dade0c7298c83ec2
Nick Mitchell <starpit@users.noreply.github.com>
2018-04-13 18:22:30 -0400

    Update shellConfig.js

--- shellConfig.js
+++ shellConfig.js
@@ -1,8 +1,11 @@
 {
-	"Fun Translations": {
-		"instructions": "This example uses the Shakespeare API from Fun Translations. Enter the API Key below. If you do not enter a key, the usage limit is 5 calls/day.",
-		"required": false,
-		"source": "http://funtranslations.com/api#shakespeare",
-		"input": [{"name": "ftApiKey", "label": "API Key"}]
+	"description": "This composition translates utterances to and from Twilio",
+	"choices": {
+		"Fun Translations": {
+			"instructions": "This example uses the Shakespeare API from Fun Translations. Enter the API Key below. If you do not enter a key, the usage limit is 5 calls/day.",
+			"required": false,
+			"source": "http://funtranslations.com/api#shakespeare",
+			"input": [{"name": "ftApiKey", "label": "API Key"}]
+		}
 	}
 }
