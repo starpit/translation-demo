0dbc0b1772f40d050f719af93b8462e3a2e39122
Nick Mitchell <starpit@users.noreply.github.com>
2018-04-14 18:29:44 -0400

    Update shellConfig.js

--- shellConfig.js
+++ shellConfig.js
@@ -1,5 +1,5 @@
 {
-	"description": "This composition translates utterances to and from Twilio",
+	"description": "This composition translates English utterances to Shakespeare-style English, and translates any other utterance to plain English. It uses the Fun Translation service for the former, and the Watson Language Translation service for the latter; the Watson Language Identification service helps us determine the uttered language.",
 	"choices": {
 		"Fun Translations": {
 			"instructions": "This example uses the Shakespeare API from Fun Translations. Enter the API Key below. If you do not enter a key, the usage limit is 5 calls/day.",
