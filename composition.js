de0887e4c43cb0554f07a2f90dc26aa55f899a49
Nick Mitchell <starpit@users.noreply.github.com>
2018-04-13 20:10:41 -0400

    Update composition.js

--- composition.js
+++ composition.js
@@ -1,15 +1,15 @@
 composer.try(
 	composer.sequence(
-		`/${namespace}/watson-language/languageId`,
+		'watson-language/languageId',
 		composer.if(
 			p => p.language !== 'en',
 			composer.sequence(
 				p => ({translateFrom: p.language, translateTo: 'en', payload: p.payload}),
-				`/${namespace}/watson-language/translator`
+				'watson-language/translator'
 			),
 			composer.sequence(
 				p => ({text: p.payload}),
-				`/${namespace}/${projectName}/en2shakespeare`
+				'translation-demo/en2shakespeare'
 			)			
 		)
 	),
