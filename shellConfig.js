1981172318e0086192c5bb330076029fcc2f5aa4
Nick Mitchell <starpit@users.noreply.github.com>
2018-04-14 21:38:45 -0400

    Update shellConfig.js

--- shellConfig.js
+++ shellConfig.js
@@ -7,6 +7,7 @@
 			"instructions": "This example uses the Shakespeare API from Fun Translations. Enter the API Key below. If you do not enter a key, the usage limit is 5 calls/day.",
 			"required": false,
 			"source": "http://funtranslations.com/api#shakespeare",
+			"registration": "http://funtranslations.com/register",
 			"input": [{"name": "ftApiKey", "label": "API Key"}]
 		}
 	}
