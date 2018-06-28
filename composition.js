7a11de250b0a6b84fe53f84cdc859abbd23deca6
Nick Mitchell <nickm@us.ibm.com>
2018-06-27 20:57:40 -0400

    update to composer v6

--- composition.js
+++ composition.js
@@ -1,17 +1,12 @@
-composer.try(
-	composer.sequence(
-		'watson-language/languageId',
-		composer.if(
-			p => p.language !== 'en',
-			composer.sequence(
-				p => ({translateFrom: p.language, translateTo: 'en', payload: p.payload}),
-				'watson-language/translator'
-			),
-			composer.sequence(
-				p => ({text: p.payload}),
-				'lib/en2shakespeare'
-			)			
-		)
-	),
-	err => ({payload:'Sorry we cannot translate your text'})
-)
+const { if: If, try: Try, sequence: Seq } = require('@ibm-functions/composer')
+
+module.exports = Try(
+    /* try */ Seq('watson-language/languageId',
+		  If( /* cond */ p => p.language !== 'en',
+		      /* then */ Seq(p => ({translateFrom: p.language,
+                                            translateTo: 'en', payload: p.payload}),
+			             'watson-language/translator'),
+		      /* else */ Seq(p => ({text: p.payload}),
+			             'lib/en2shakespeare'))),
+
+    /* catch */ err => ({payload:'Sorry we cannot translate your text'}))
